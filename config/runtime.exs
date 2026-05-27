0b92a16f33a41064a565d4722fe23ee588ed193e
sljuka <david.sljukic.dev@gmail.com>
2026-05-27 23:03:26 +0200

    feat(deploy): remove extra option

--- config/runtime.exs
+++ config/runtime.exs
@@ -34,7 +34,6 @@ if config_env() == :prod do
 
   config :cool, Cool.Repo,
     ssl: true,
-    ssl_opts: [verify: :verify_name],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     # For machines with several cores, consider starting multiple pools of `pool_size`
